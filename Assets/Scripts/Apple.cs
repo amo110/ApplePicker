d08bc5f1b6e5f2d1a69d097d85a347425e03d61e
amo110 <abiolaoyelaja@nbp-201-82.nbp.ruw.rutgers.edu>
2015-10-11 13:44:53 -0400

    Finished

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : MonoBehaviour {
+
+	public static float bottomY = -20f;
+
+	void Update () {
+		if (transform.position.y < bottomY) {
+			Destroy(this.gameObject);
+		
+			Apple_Picker apScript = Camera.main.GetComponent<Apple_Picker>();
+			apScript.AppleDestroyed();
+		}
+}
+}
---
+++ Assets/Scripts/Apple_Tree.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple_Tree : MonoBehaviour {
+
+	public GameObject applePrefab;
+	public float speed = 1f;
+	public float leftAndRightEdge = -20f;
+	public float chanceToChangeDirections = 0.02f;
+	public float secondsBetweenAppleDrops = 1f;
+	void Start () {
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+	void DropApple(){
+		GameObject apple = Instantiate (applePrefab) as GameObject;
+		apple.transform.position = transform.position;
+	}
+	void Update (){
+	
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed);
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed);
+		} else if (Random.value < chanceToChangeDirections) {
+			speed *= -Mathf.Abs (speed);
+		}
+	}
+	void FixedUpdate (){
+		if (Random.value < chanceToChangeDirections){
+			speed *= -1;
+
+
+}
+	}
+}
